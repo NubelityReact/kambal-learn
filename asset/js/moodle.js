1ff635ab3ce479c41f93da5ba9222202e74d9689
Bernabe Gutierrez Rodriguez <bernabe.gutierrez.rodriguez@gmail.com>
2024-07-11 01:05:56 +0000

    Update moodle.js

--- asset/js/moodle.js
+++ asset/js/moodle.js
@@ -51,6 +51,21 @@ function error_user_create_moodle(message) {
     swalert('Moodle dice:', message, 'info');
 }
 
+function core_user_update_institut(idimoodle, institution) {
+    //swalert('Mensaje!', 'Procesando...', 'info');
+    $.ajax({
+        type: "POST",
+        url: "dataConect/moodle.php",
+        data: 'action=core_user_update_institut&idimoodle=' + idimoodle + '&institution=' + institution,
+        success: function (text) {
+      //      swalert('Mensaje!', text, 'info');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+        //    swalert('Mensaje!', jqXHR + " " + textStatus + " " + errorThrown, 'warning');
+        }
+    });
+}
+
 function core_user_update_password(idimoodle, new_password) {
     swalert('Mensaje!', 'Procesando...', 'info');
     $.ajax({
@@ -59,6 +74,55 @@ function core_user_update_password(idimoodle, new_password) {
         data: 'action=core_user_update_password&idimoodle=' + idimoodle + '&new_password=' + new_password,
         success: function (text) {
             swalert('Mensaje!', text, 'info');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            swalert('Mensaje!', jqXHR + " " + textStatus + " " + errorThrown, 'warning');
+        }
+    });
+}
+
+function auth_userkey_request_login_url(moodle_email) {
+    var loginurl;
+    $.ajax({
+        type: "POST",
+        url: "dataConect/moodle.php",
+        data: "action=auth_userkey_request_login_url&moodle_email=" + moodle_email,
+        dataType: 'json',
+        async: false,
+        success: function (text) {
+            //var myObj = JSON.stringify(text);
+            //var date = text.data;
+            loginurl = text.data.loginurl;
+            //return (text.data.loginurl);
+            return (loginurl);
+            console.log(text.data.loginurl);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            loginurl = jqXHR;
+        }
+    });
+    return(loginurl);
+}
+
+function isValidURL(url) {
+    var RegExp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+
+    if (RegExp.test(url)) {
+        return true;
+    } else {
+        return false;
+    }
+} 
+
+function core_enrol_manual_users(userid, courseid, idigenerales) {
+    //swalert('Mensaje!', 'Procesando...', 'info');
+    $.ajax({
+        type: "POST",
+        url: "dataConect/moodle.php",
+        data: 'action=enrol_manual_enrol_users&userid=' + userid + '&courseid=' + courseid,
+        success: function (text) {
+            console.log('enrol_manual_enrol_users ' + text);
+            //updateidmoodle(userid, idigenerales);
         },
         error: function (jqXHR, textStatus, errorThrown) {
             swalert('Mensaje!', jqXHR + " " + textStatus + " " + errorThrown, 'warning');
